3aed46beda7a831039271aae162c2c85f5c169a2
gustavo-nomelini <gustavolnomelini@gmail.com>
2026-09-05 21:21:36 -0300

    Initial Commit

---
+++ apple-memory-management/apple_memory_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_memory_managementApp.swift
+//  apple-memory-management
+//
+//  Created by Gustavo Nomelini on 05/09/26.
+//
+
+import SwiftUI
+
+@main
+struct apple_memory_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
